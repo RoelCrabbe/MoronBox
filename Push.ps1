a17bf410ce6596ed8e8dc377d5fed305237cf098
Roel Crabbe <crabberoel@gmail.com>
2024-06-20 16:04:37 +0200

    Update submodules and repositories

--- Push.ps1
+++ Push.ps1
@@ -1,23 +1,47 @@
 # Get the current directory where the script is located
 $targetDirectory = $PSScriptRoot
 
-# Remote repository URL where you want to push changes
-$remoteRepository = "combined_repo"
+# Array of repositories with directory names and submodule paths
+$repositories = @(
+    @{ Url = "https://github.com/RoelCrabbe/MoronBoxCore.git"; DirectoryName = "MoronBoxCore"; SubmodulePath = "MoronBoxCore" },
+    @{ Url = "https://github.com/zelazrgn/KLHThreatMeter.git"; DirectoryName = "KLHThreatMeter"; SubmodulePath = "KLHThreatMeter" },
+    @{ Url = "https://github.com/shagu/ShaguTweaks.git"; DirectoryName = "ShaguTweaks"; SubmodulePath = "ShaguTweaks" }
+)
 
-# Function to push changes
-function Push-Changes {
+# Function to update each repository including submodules
+function Update-Repositories {
     param (
-        [string]$remoteName = "origin",
-        [string]$branchName = "master"
+        [string]$directoryName,
+        [string]$submodulePath
     )
 
-    Write-Output "Pushing changes to $remoteName/$branchName"
-    git push $remoteName $branchName
+    Write-Output "Updating repository: $directoryName"
+    Set-Location $directoryName
+    git pull
+    
+    # Update submodules if any
+    if (Test-Path .gitmodules) {
+        git submodule update --init --recursive
+    }
+
+    Set-Location $targetDirectory
 }
 
-Write-Output "Started pushing changes from $targetDirectory."
+Write-Output "Started updating repositories and submodules in $targetDirectory."
 
-# Push changes from the main repository
-Push-Changes
+# Loop through each repository in the array
+foreach ($repoInfo in $repositories) {
+    $directoryName = $repoInfo.DirectoryName
+    $submodulePath = $repoInfo.SubmodulePath
 
-Write-Output "Changes pushed successfully to $remoteRepository."
+    # Call function to update repository and submodules
+    Update-Repositories -directoryName $directoryName -submodulePath $submodulePath
+}
+
+Write-Output "Repositories and submodules updated in $targetDirectory."
+
+# Push changes to the main repository
+Write-Output "Pushing changes to main repository in $targetDirectory."
+git add .
+git commit -m "Update submodules and repositories"
+git push
