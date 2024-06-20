a962ddf76e10f3bfa633d81e3adc6786747a8a7b
Roel Crabbe <crabberoel@gmail.com>
2024-06-20 15:43:31 +0200

    Added clone and pull scipts

---
+++ Clone.ps1
@@ -0,0 +1,24 @@
+# Get the current directory where the script is located
+$targetDirectory = $PSScriptRoot
+
+# Array of repositories to clone with directory names
+$repositories = @(
+    @{ Url = "https://github.com/RoelCrabbe/MoronBoxCore.git"; DirectoryName = "MoronBoxCore" },
+    @{ Url = "https://github.com/zelazrgn/KLHThreatMeter.git"; DirectoryName = "KLHThreatMeter" },
+    @{ Url = "https://github.com/shagu/ShaguTweaks.git"; DirectoryName = "ShaguTweaks" }
+)
+
+# Loop through each repository in the array
+
+Write-Output "Started cloning into $targetDirectory."
+
+foreach ($repoInfo in $repositories) {
+    $repoUrl = $repoInfo.Url
+    $directoryName = $repoInfo.DirectoryName
+
+    # Clone the repository into the current directory
+    $clonePath = Join-Path -Path $targetDirectory -ChildPath $directoryName
+    git clone $repoUrl $clonePath
+}
+
+Write-Output "Repositories cloned into $targetDirectory."
---
+++ Pull.ps1
@@ -0,0 +1,33 @@
+# Get the current directory where the script is located
+$targetDirectory = $PSScriptRoot
+
+# Array of repositories with directory names
+$repositories = @(
+    @{ Url = "https://github.com/RoelCrabbe/MoronBoxCore.git"; DirectoryName = "MoronBoxCore" },
+    @{ Url = "https://github.com/zelazrgn/KLHThreatMeter.git"; DirectoryName = "KLHThreatMeter" },
+    @{ Url = "https://github.com/shagu/ShaguTweaks.git"; DirectoryName = "ShaguTweaks" }
+)
+
+# Function to update each repository
+function Update-Repositories {
+    param (
+        [string]$directoryName
+    )
+
+    Write-Output "Updating repository: $directoryName"
+    Set-Location $directoryName
+    git pull
+    Set-Location $targetDirectory
+}
+
+Write-Output "Started updating repositories in $targetDirectory."
+
+# Loop through each repository in the array
+foreach ($repoInfo in $repositories) {
+    $directoryName = $repoInfo.DirectoryName
+
+    # Call function to update repository
+    Update-Repositories -directoryName $directoryName
+}
+
+Write-Output "Repositories updated in $targetDirectory."
